dea25d2a90ebdf8df18f204c930b9debb5fa2c80
Tran Thi Thu Huong <huong.ttt171298@gmail.com>
2020-01-29 21:10:05 +0700

    v1

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@ const cards = document.querySelectorAll('.memory-card');
   let firstCard, secondCard;
 
   function flipCard() {
-   if (lockBoard) return;
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
     this.classList.add('flip');
 
     if (!hasFlippedCard) {
@@ -15,30 +17,33 @@ const cards = document.querySelectorAll('.memory-card');
     }
 
     secondCard = this;
-    hasFlippedCard = false;
+    lockBoard = true;
 
     checkForMatch();
   }
 
   function checkForMatch() {
-    let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
     isMatch ? disableCards() : unflipCards();
   }
 
   function disableCards() {
-    firstCard.removeEventListener('click', flipCard);
-    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
   }
 
   function unflipCards() {
-      lockBoard = true;
-
     setTimeout(() => {
       firstCard.classList.remove('flip');
       secondCard.classList.remove('flip');
 
-      lockBoard = false;
+      resetBoard();
     }, 1500);
   }
+
+  function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+  }
+
 
   cards.forEach(card => card.addEventListener('click', flipCard));

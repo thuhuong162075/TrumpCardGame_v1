0410cf9f531bc23cfb01fdcb08a1fb511760983b
Tran Thi Thu Huong <huong.ttt171298@gmail.com>
2020-02-03 22:36:25 +0700

    disappear the same card

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
 //Kiểm tra số lần click
-var click;
+var click=0;
 //Lưu this của lần click 1
 var ctrl1;
 //lưu this của lần click 2
@@ -9,7 +9,7 @@ var location1;
 //lưu vị trí của lần click 2
 var location2;
 //mảng ảnh 
-var mang=[];
+var array_img=[];
 // lưu số tấm thẻ được lật lên 
 var card_flip=0;
 
@@ -24,16 +24,59 @@ function createGame(){
 // tạo mảng lưu ảnh 
 function NextLevel(){
 	for(var i=1;i<=2;i++){
-		mang.push("img/pic"+i+".png","img/pic"+i+".png");
+		array_img.push("img/pic"+i+".png","img/pic"+i+".png");
 	}
 }
 //tạo hàm in giao diện 
 function printInterface(){
 	NextLevel();
 	var shtml="";
-	for(var i=0;i<mang.length;i++){
-		shtml = shtml + "<div class='card'><img class='off' id='off"+i+"' onclick='ChonAnh(this,"+i+");' width='100%' src='img/pic0.png'><img class='on' id='on"+i+"'src='"+mang[i]+"'></div>"
+	for(var i=0;i<array_img.length;i++){
+		shtml = shtml + "<div class='card'><img class='off' id='off"+i+"' onclick='Selec_img(this,"+i+");' width='100%' src='img/pic0.png'><img class='on' id='on"+i+"'src='"+array_img[i]+"'></div>"
 	}
 	$('#main').html(shtml);
 	$('#card_flip').html("<span>Cards Flipped: "+card_flip+"</span>")
+}
+
+function Selec_img(ctrl,i){
+	if(click==2) return;
+	if(click==0){
+		ctrl1=ctrl;
+		location1=i;
+		$(ctrl1).css('transform','rotateY(180deg)');
+		$("#on"+location1).css('transform','rotateY(0deg)');
+		click=1;
+	}else{
+		ctrl2=ctrl;
+		location2=i;
+		$(ctrl2).css('transform','rotateY(180deg)');
+		$("#on"+location2).css('transform','rotateY(0deg)');
+		click=2;
+		if(ctrl1 != ctrl2){
+			setTimeout(Check,600);
+		}else{
+			click=1;
+			return;
+		}
+	}
+}
+function Check(){
+	click=0;
+	if(array_img[location1] == array_img[location2]){
+		$('#on'+location1).css('visibility','hidden');
+		$('#off'+location1).css('visibility','hidden');
+		$('#on'+location2).css('visibility','hidden');
+		$('#off'+location2).css('visibility','hidden');
+		card_flip++;
+		$('#card_flip').html("<span>Cards Flipped: "+card_flip+"</span>")
+		
+	}else {
+		$("#off"+location1).css('transform','rotateY(0deg)');
+		$("#off"+location2).css('transform','rotateY(0deg)');
+		$("#on"+location1).css('transform','rotateY(180deg)');
+		$("#on"+location2).css('transform','rotateY(180deg)');	
+	}
+	if(card_flip==array_img.length/2){
+			alert('WIN');
+		}
 }
